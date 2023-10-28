902090bd0119357f4af6662a235d45ff030b7c4d
yoppy-h <h2210508@gl.cc.uec.ac.jp>
2023-10-28 16:41:58 +0900

    miss

--- example/chrome-basics/popup.js
+++ example/chrome-basics/popup.js
@@ -1,15 +1,34 @@
+let gifWindow = null;
+let isPlaying = false;
+
 document.getElementById('btn').addEventListener('click', () => {
-    document.querySelector('h1').textContent = "CHANGED !!";
+    if (!gifWindow) {
+        // ウィンドウが存在しない場合、新しいウィンドウを作成
+        //gifWindow = window.open('whip.gif', 'GifWindow', 'width=300,height=300,top=0,left=0,resizable=no,movable=no,scrollbars=no,toolbar=no,menubar=no,location=no');
+        gifWindow = window.open('whip.gif', 'GifWindow', 'width=300,height=300,top=0,left=0,resizable=no');
+        gifWindow.blur();
+        gitWindow.focus();
+        isPlaying = true;
+    } else {
+        // ウィンドウが既に存在する場合、再生を停止してウィンドウを閉じる
+        if (isPlaying) {
+            // gifを停止する処理を実行（例えば、gifWindow.stop()など）
+			gifWindow.stop();
+        }
+        gifWindow.close();
+        gifWindow = null;
+        isPlaying = false;
+    }
 });
 
 document.getElementById('btn_get').addEventListener('click', () => {
-    chrome.tabs.query( {active:true, currentWindow:true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {message: 'getname'}, (content) => {
-            if(!content){
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { message: 'getname' }, (content) => {
+            if (!content) {
                 alert('Cannot Get! Try Reload First!');
                 return;
             }
-            document.getElementById('title').value = content
+            document.getElementById('title').value = content;
         });
     });
 });
