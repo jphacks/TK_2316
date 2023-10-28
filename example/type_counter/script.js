32c97cfdf69601d78c5b5acaf7998bc88fad9d8b
yoppy-h <h2210508@gl.cc.uec.ac.jp>
2023-10-28 21:05:48 +0900

    javascriptでキーの検知とカウントのプログラム
    コメントは適当なので気にしないでください。

---
+++ example/type_counter/script.js
@@ -0,0 +1,19 @@
+// カウンター変数を初期化
+let counter = 0;
+
+// キーが押された瞬間（keydown）の処理
+document.addEventListener("keydown", function(event) {
+    // イベントオブジェクトから押されたキーの情報を取得
+    let key = event.key;
+    let type_count = document.getElementById('type_count');
+    let type_key = document.getElementById('type_key');
+    // カウンターを1増加
+    counter++;
+    
+    //IDがtargetの要素のテキストを更新
+    type_count.innerText = "文字数:" + counter;
+    type_key.innerText = "入力キー:" + key;
+
+    //alert(counter);
+
+});
