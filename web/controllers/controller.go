7051693e4a48827d424e235052b28525d47f5e1c
shiori-42 <shiori0123.happysmile@gmail.com>
2023-10-29 09:51:37 +0900

    add

--- web/controllers/controller.go
+++ web/controllers/controller.go
@@ -1,23 +1,35 @@
 package controllers
 
 import (
+    "fmt"
     "net/http"
     "web/models"
     "github.com/gin-gonic/gin"
 )
 
-
 func GetRanking(c *gin.Context) {
     rankings, err := models.GetRanking()
+    fmt.Printf("%v\n",rankings)
     if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to fetch ranking"})
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to fetch ranking", "details": err.Error()})
         return
     }
     c.HTML(http.StatusOK, "ranking.html", gin.H{"users": rankings})
 }
 
+func PostRanking(c *gin.Context) {
+    var newRanking models.Ranking
 
+    if err := c.BindJSON(&newRanking); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input", "details": err.Error()})
+        return
+    }
 
+    err := models.UpdateOrCreateRanking(&newRanking)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update or create ranking", "details": err.Error()})
+        return
+    }
 
-
-
+    c.JSON(http.StatusOK, newRanking)
+}
--- web/main.go
+++ web/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-		"web/models"
-		"web/routes"
+	"log"
+	"web/models"
+	"web/routes"
+	"github.com/spf13/viper"
 )
 
-func main() {
+func initConfig() error {
+    viper.SetConfigName("config")  // 設定ファイルの名前（拡張子なし）
+    viper.AddConfigPath("/mnt/c/Users/Student/TK_2316/")  // 設定ファイルのパスを追加
+    return viper.ReadInConfig()  // 設定ファイルを読み込む
+}
 
-	models.ConnectDatabase()
+func main() {
+    err := initConfig()  // 設定を初期化
+    if err != nil {
+        log.Fatalf("Fatal error config file: %s", err)
+    }
+    
+    host := viper.GetString("server.host")  // 設定からホストを取得
+    port := viper.GetInt("server.port")     // 設定からポートを取得
+    
+    log.Println(host, port)  // 値をロギング
+
+	err = models.ConnectDatabase()
+    if err != nil {
+        log.Fatalf("Could not connect to database: %v", err)
+    }
 
     models.Migrate()
 
